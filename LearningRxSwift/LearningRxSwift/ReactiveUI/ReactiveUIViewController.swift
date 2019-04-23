27d26ad804970d8dc2eb7fd2f3a9c6365b45a9de
BergQuester <daniel.bergquist@gmail.com>
2019-04-23 11:18:08 -0600

    Ch 3-2: Changine UI: Subscription, bindTo, driver

--- LearningRxSwift/LearningRxSwift/ReactiveUI/ReactiveUIViewController.swift
+++ LearningRxSwift/LearningRxSwift/ReactiveUI/ReactiveUIViewController.swift
@@ -5,12 +5,14 @@ import RxSwift
 import RxCocoa
 
 class ReactiveUIViewController: UIViewController {
+    
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var tableView: UITableView!
     @IBOutlet var acceptButton: UIButton!
     
     fileprivate var presenter = ReactiveUIPresenter() //normally injected as an interface
-    
+    fileprivate var bag = DisposeBag()
+
     var mainThreadPointer: Thread!
     
     override func viewDidLoad() {
@@ -20,6 +22,8 @@ class ReactiveUIViewController: UIViewController {
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Default")
         tableView.dataSource = self
+
+        rxExamples()
     }
     
     @IBAction func acceptTapped(_ sender: UIButton) {
@@ -29,7 +33,35 @@ class ReactiveUIViewController: UIViewController {
 
 
 
-//Parts 1 & 2
+//MARK: rxExamples()
+extension ReactiveUIViewController {
+    func rxExamples() {
+        rxTitle()
+    }
+
+    func rxTitle() {
+//        // subscription
+//        presenter.title.asObservable()
+//                       .observeOn(MainScheduler.instance)
+//                       .subscribe(onNext: { [weak self] title in
+//                            self?.titleLabel.text = title
+//
+//                       }).disposed(by: bag)
+
+        // Bind to
+//        presenter.title.asObservable().bind(to: titleLabel.rx.text).disposed(by: bag)
+//        presenter.title.asObservable()
+////                       .observeOn(MainScheduler.instance)
+////                       .catchErrorJustReturn("Default Value")
+////                       .share(replay: 1, scope: .forever)
+//                       .bind(to: titleLabel.rx.text)
+//                       .disposed(by: bag)
+
+        presenter.title.asDriver(onErrorJustReturn: "Default Value")
+                       .drive(titleLabel.rx.text)
+                       .disposed(by: bag)
+    }
+}
 
 //MARK: - UITableViewDataSource
 extension ReactiveUIViewController: UITableViewDataSource {
